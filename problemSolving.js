b7e62ea070aef9974ab503e649d024b7cebe5e08
Nobab <nobabsarkar2016@gmail.com>
2023-02-06 08:04:09 -0800

    add new code

--- problemSolving.js
+++ problemSolving.js
@@ -1,9 +1,20 @@
 // 1. covert radian to degree
-
 function radianToDegree(radianToDegree) {
-  const radian = radianToDegree * 57.2958;
-  return radian;
+  const convertRadianToDegree = radianToDegree * 57.2958;
+  return convertRadianToDegree;
+}
+const result = radianToDegree(3);
+// console.log(result);
+
+// 2. check whether the given file name is a javascript file or not
+function isJavascriptFile(check) {
+  const fileExtension = check.split(".").pop();
+  if (fileExtension == "js") {
+    return true;
+  } else {
+    return "Please write correct file format";
+  }
 }
 
-const result = radianToDegree(3);
-console.log(result);
+const results = isJavascriptFile("assignment.js");
+console.log(results);
