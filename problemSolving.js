e68e8722e679756f5dee1c0eb2571919e97dadbb
Nobab <nobabsarkar2016@gmail.com>
2023-02-06 07:53:30 -0800

    add new code

--- problemSolving.js
+++ problemSolving.js
@@ -1,1 +1,9 @@
-const hello = 10;
+// 1. covert radian to degree
+
+function radianToDegree(radianToDegree) {
+  const radian = radianToDegree * 57.2958;
+  return radian;
+}
+
+const result = radianToDegree(3);
+console.log(result);
